ec62a6ab494d8254ea64dd88c1416d3985fc19bf
StevenNguyen0603 <vo.nguyen@moovapp.vn>
2026-06-05 11:07:54 +0700

    Add background music tracks for player functionality

---
+++ src/data/backgroundMusic.js
@@ -0,0 +1,62 @@
+export const BACKGROUND_MUSIC_TRACKS = [
+  {
+    title: 'Magic in the Air',
+    artist: 'Magic System',
+    src: '/magic-in-the-air.mp3',
+  },
+  {
+    title: 'Oh Africa',
+    artist: 'Akon featuring Keri Hilson',
+    src: '/oh-africa.mp3',
+  },
+  {
+    title: 'Dreamers',
+    artist: 'Jung Kook featuring Fahad Al Kubaisi',
+    src: '/dreamers.mp3',
+  },
+  {
+    title: 'Hayya Hayya',
+    artist: 'Trinidad Cardona, Davido & Aisha',
+    src: '/hayya-hayya.mp3',
+  },
+  {
+    title: 'We Are One',
+    artist: 'Pitbull, Jennifer Lopez & Claudia Leitte',
+    src: '/we-are-one.mp3',
+  },
+  {
+    title: 'Wavin Flag',
+    artist: "K'naan",
+    src: '/wavin-flag.mp3',
+  },
+  {
+    title: 'The Cup of Life',
+    artist: 'Ricky Martin',
+    src: '/the-cup-of-life.mp3',
+  },
+  {
+    title: 'Waka Waka',
+    artist: 'Shakira',
+    src: '/waka-waka.mp3',
+  },
+  {
+    title: "Hips Don't Lie",
+    artist: 'Shakira',
+    src: '/hips-dont-lie.mp3',
+  },
+  {
+    title: 'Dai Dai',
+    artist: 'Youssou N Dour & Axelle Red',
+    src: '/dai-dai.mp3',
+  },
+  {
+    title: 'Bamboo',
+    artist: 'Pitbull',
+    src: '/bamboo.mp3',
+  },
+  {
+    title: 'La La La',
+    artist: 'Shakira featuring Carlinhos Brown',
+    src: '/lalala.mp3',
+  },
+]
